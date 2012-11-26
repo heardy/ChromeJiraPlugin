250006d00e8d294060a4d7505d5a0ad34603bbd4
Kevin Heard <kevin.heard@massiveinteractive.com>
2012-11-26 15:33:51 +1100

    initial commit

---
+++ background.js
@@ -0,0 +1,56 @@
+function debug(msg)
+{
+	//console.log("JIRA Plugin: " + msg)
+}
+
+var BILLINGKEY_ID = "tempo-expenditure-item",
+	FILENAME = "CreateIssue.jspa",
+	PROJECTNAME_ID = "issue-create-project-name";
+
+var PROJECTS = {
+	"TAB" : "TAB IPTV"
+};
+
+JIRA_PLUGIN = (function(){
+	start = function()
+	{
+		debug("start plugin");
+		var projectName = "",
+			projectNameEL = document.getElementById(PROJECTNAME_ID),
+			billingKeyEL = document.getElementById(BILLINGKEY_ID);
+
+		if (projectNameEL === null)
+		{
+			// return;
+		}
+		else
+		{
+			projectName = projectNameEL.textContent;
+			debug(projectName);
+		}
+
+		if (billingKeyEL === null)
+		{
+			return;
+		}
+
+		if (location.pathname.indexOf(FILENAME) >= 0)
+		{
+			switch(projectName)
+			{
+				case PROJECTS.TAB:
+					debug("PROJECTS.TAB=" + PROJECTS.TAB);
+					billingKeyEL.selectedIndex = 2;
+				break;
+			}
+		}
+	};
+
+	return {
+		init: function(){
+			start();
+		}
+	};
+})();
+
+document.addEventListener( "DOMContentLoaded", JIRA_PLUGIN.init, false );
